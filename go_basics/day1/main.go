c107ce75ca6fa50e954619ec35c514ad1986183e
Amar Singh Rathour <singhamarsingh555@gmail.com>
2025-12-03 09:24:51 +0530

    initialize Go module and add basic Hello World program

---
+++ go_basics/day1/main.go
@@ -0,0 +1,29 @@
+// Description: A simple Go program that prints "Hello, World!" to the console.
+package main
+
+// Importing the fmt package for formatted I/O
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// go run = compile and run the code
+// go build = compile the code into an executable file you can run later
+// go mod init <module_name> = initialize a new Go module
+//go.mod = keeps track of your module's dependencies and versions (like a project identity card
+
+//Homework:
+//Create a new Go program called aboutme.go that prints:
+//
+//Hi, I'm Ananya.
+//I'm learning Go programming.
+//Go is awesome!
+//Run the program to see the output!
+//Bonus: Modify the program to include your favorite programming language and why you like it.
+//Example Output:
+//
+//Hi, I'm Ananya.
+//I'm learning Go programming.
+//Go is awesome!
+//My favorite programming language is Python because of its simplicity and versatility.
